5bd6715be7b9d2ed706974a34cb6e4f81c66142f
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-12-30 15:59:22 +0200

    RouteFactory default to base Routes if not found

--- src/RouteFactory.php
+++ src/RouteFactory.php
@@ -2,100 +2,13 @@
 
 namespace Nip\Router;
 
+use Nip\Router\RouteFactory\HasBaseRoutesTrait;
+
 /**
  * Class RouteCollection
  * @package Nip\Router
  */
 class RouteFactory
 {
-
-    /**
-     * @param RouteCollection $collection
-     * @param $name
-     * @param $class
-     * @param string $mapPrefix
-     * @return mixed
-     */
-    public static function generateIndexRoute(
-        $collection,
-        $name,
-        $class,
-        $mapPrefix = ''
-    ) {
-        $params = ["controller" => "index", "action" => "index"];
-
-        self::generateLiteralRoute(
-            $collection, $name.'.slash', $class, $mapPrefix, '', $params
-        );
-
-        return self::generateLiteralRoute(
-            $collection, $name, $class, $mapPrefix, '/', $params
-        );
-    }
-
-    /**
-     * @param RouteCollection $collection
-     * @param $name
-     * @param $class
-     * @param string $mapPrefix
-     * @param string $map
-     * @param array $params
-     * @return mixed
-     */
-    public static function generateLiteralRoute(
-        $collection,
-        $name,
-        $class,
-        $mapPrefix = '',
-        $map = '/',
-        $params = []
-    ) {
-        $map = $mapPrefix . $map;
-
-        return self::generateGenericRoute($collection, $name, $class, $map, $params);
-    }
-
-    /**
-     * @param RouteCollection $collection
-     * @param $name
-     * @param $class
-     * @param string $map
-     * @param array $params
-     * @return mixed
-     */
-    public static function generateGenericRoute(
-        $collection,
-        $name,
-        $class,
-        $map,
-        $params = []
-    ) {
-        $map = str_replace('//', '/', $map);
-
-        $route = new $class($map, $params);
-        return $collection->addRoute($route, $name);
-    }
-
-    /**
-     * @param RouteCollection $collection
-     * @param $name
-     * @param $class
-     * @param string $mapPrefix
-     * @param string $map
-     * @param array $params
-     * @return mixed
-     */
-    public static function generateStandardRoute(
-        $collection,
-        $name,
-        $class,
-        $mapPrefix = '',
-        $map = '/{controller}/{action?index}',
-        $params = []
-    ) {
-        self::generateGenericRoute($collection, $name, $class, $mapPrefix . $map, $params);
-        
-        $params['action'] = 'index';
-        return self::generateGenericRoute($collection, $name.'.index', $class, $mapPrefix . '/{controller}/', $params);
-    }
+    use HasBaseRoutesTrait;
 }

af8f4887cf8e30a3205a3479bcb6c9f192c225ef
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-07-06 20:18:17 +0300

    update default route case insensive

--- src/Generator/Traits/HasDefaultRouteTrait.php
+++ src/Generator/Traits/HasDefaultRouteTrait.php
@@ -23,7 +23,7 @@ trait HasDefaultRouteTrait
     /**
      * @param $name
      * @param array $params
-     * @return null|Route
+     * @return string
      */
     protected function initDefaultRoute($name, &$params = [])
     {
@@ -31,18 +31,31 @@ trait HasDefaultRouteTrait
         if ($route) {
             return $name;
         }
+        return $this->initMvcRoute($name, $params);
+    }
+
+    /**
+     * @param $name
+     * @param array $params
+     * @return string
+     */
+    protected function initMvcRoute($name, &$params = [])
+    {
         $parts = explode(".", $name);
         $count = count($parts);
-        if ($count <= 3) {
-            if (in_array(reset($parts), app('mvc.modules')->getNames())) {
-                $module = array_shift($parts);
-                $params['controller'] = isset($parts[0]) ? $parts[0] : null;
-                $params['action'] = isset($parts[1]) ? $parts[1] : null;
-                $defaultRoute = $module . '.default';
-                if ($this->hasRoute($defaultRoute)) {
-                    return $defaultRoute;
-                }
-            }
+        if ($count > 3) {
+            return $name;
+        }
+        $firstPart = strtolower(reset($parts));
+        if (!in_array($firstPart, app('mvc.modules')->getNames())) {
+            return $name;
+        }
+        $module = array_shift($parts);
+        $params['controller'] = isset($parts[0]) ? $parts[0] : null;
+        $params['action'] = isset($parts[1]) ? $parts[1] : null;
+        $defaultRoute = $module . '.default';
+        if ($this->hasRoute($defaultRoute)) {
+            return $defaultRoute;
         }
         return $name;
     }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,8 @@ require dirname(__DIR__).'/vendor/autoload.php';
 define('PROJECT_BASE_PATH', __DIR__ . '/..');
 define('TEST_BASE_PATH', __DIR__);
 define('TEST_FIXTURE_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures');
+
+$container = new \Nip\Container\Container();
+\Nip\Container\Container::setInstance($container);
+
+$container->set('inflector',new \Nip\Inflector\Inflector());

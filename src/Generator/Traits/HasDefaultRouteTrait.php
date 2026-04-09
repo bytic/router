695a61244e258092d0955fc63fb9e8ec73152ee2
Gabriel Solomon <hello@gabrielsolomon.ro>
2026-04-09 11:45:20 +0300

    feat: update webViewContext and add GitToolBoxBlameSettings

--- src/Generator/Traits/HasDefaultRouteTrait.php
+++ src/Generator/Traits/HasDefaultRouteTrait.php
@@ -16,6 +16,7 @@ trait HasDefaultRouteTrait
     public function generate(string $name, array $parameters = [], int $referenceType = self::ABSOLUTE_PATH): string
     {
         $name = $this->initDefaultRoute($name, $parameters);
+        $parameters['action'] = $parameters['action'] ?? 'index';
         return parent::generate($name, $parameters, $referenceType);
     }
 
